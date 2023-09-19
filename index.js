6edd4019dcece5ec863479979fc634c712ebce7e
githinjisamson1 <githinjisamson148@gmail.com>
2023-09-19 23:12:12 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function () {
+  return 2000 / 100;
+};
+
+const square = (numToSquare) => {
+  return numToSquare ** 2;
+};
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
